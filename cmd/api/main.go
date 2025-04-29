6003078d5b4a1c767f8210afa00f3e50e2227e20
lourencogabe <gabriels211@hotmail.com>
2025-04-28 23:48:14 -0300

    feat: adds CORS politics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	infra "github.com/lourencogabe/gonimbus/infra/handlers"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	infra.GetWeathersRoute(router)
 	router.Run("localhost:8080")
 }
